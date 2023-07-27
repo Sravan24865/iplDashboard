3ff01f27ff9d73bbaedf4a6ee4af4aacf52ab462
Sravan <sravanamjala@gmail.com>
2023-07-27 11:36:34 +0000

    IPL Dashboard

---
+++ src/components/Home/index.js
@@ -0,0 +1,68 @@
+// Write your code here
+import {Component} from 'react'
+import './index.css'
+import Loader from 'react-loader-spinner'
+import TeamCard from '../TeamCard'
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+
+class Home extends Component {
+  state = {
+    teamsData: [],
+    isLoading: true,
+  }
+
+  componentDidMount() {
+    this.getTeamsList()
+  }
+
+  getTeamsList = async () => {
+    const response = await fetch('https://apis.ccbp.in/ipl')
+    const fetchData = await response.json()
+    const updatedData = fetchData.teams.map(eachData => ({
+      name: eachData.name,
+      teamImageUrl: eachData.team_image_url,
+      id: eachData.id,
+    }))
+    this.setState({teamsData: updatedData, isLoading: false})
+  }
+
+  renderTeamsList = () => {
+    const {teamsData} = this.state
+    return (
+      <ul className="team-list-items">
+        {teamsData.map(team => (
+          <TeamCard key={team.id} teamData={team} />
+        ))}
+      </ul>
+    )
+  }
+
+  renderLoader = () => (
+    // eslint-disable-next-line react/no-unknown-property
+    <div className="loader-container" testid="loader">
+      <Loader type="Rings" color="#00bfff" height={80} width={80} />
+    </div>
+  )
+
+  render() {
+    const {isLoading} = this.state
+
+    return (
+      <div className="app-container">
+        <div className="ipl-container">
+          <div className="header-container">
+            <img
+              className="ipl-logo"
+              alt="ipl logo"
+              src="https://assets.ccbp.in/frontend/react-js/ipl-logo-img.png"
+            />
+            <h1 className="header-heading">IPL Dashboard</h1>
+          </div>
+          {isLoading ? this.renderLoader() : this.renderTeamsList()}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Home
